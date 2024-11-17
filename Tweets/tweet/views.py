d677b44a78905eb3cf6127596006f376c26eb594
arya <aryapawar92@gmail.com>
2024-11-17 15:03:17 +0530

    navbar and model done

---
+++ Tweets/tweet/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm
+
+# Create your views here.
+
+def index(request):
+    return render(request , 'index.html')
